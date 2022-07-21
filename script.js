12004c2642fb7382218fbe048299e422952dad13
Vicky AV <vickyavw.10@gmail.com>
2022-07-21 20:45:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 async function exec() {
   try {
-      const first = await fetch('./data.json'); //<-- wait for the fetch to finish and return the resolved value. 
-      let data = await first.json(); //<-- use the const first, not first_name
-    
-      var htmlElements = "";
-      for (var i = 0; i < array.length; i++) {
-        htmlElements += "<div class=\"col-md-4 col-xs-6\"><canvas id=\"myChart" + i + "\" width=\"300\" height=\"300\"></canvas></div>";
-      }
-      var container = document.getElementById("row");
-      container.innerHTML = htmlElements;
-      data.forEach((e, index) => {
-        var ctx = document.getElementById('myChart' + index).getContext('2d');
-        var dataTemplate = prepareTemplate(e)
-        prepareChart(ctx, dataTemplate)
-      });
-   }
-   catch(e) {
-     console.log('Error!', e);
-   }
+    const first = await fetch('./data.json'); //<-- wait for the fetch to finish and return the resolved value. 
+    let data = await first.json(); //<-- use the const first, not first_name
+
+    var htmlElements = "";
+    for (var i = 0; i < data.length; i++) {
+      htmlElements += "<div class=\"col-md-4 col-xs-6\"><canvas id=\"myChart" + i + "\" width=\"300\" height=\"300\"></canvas></div>";
+    }
+    console.log(htmlElements)
+    var container = document.getElementById("row");
+    container.innerHTML = htmlElements;
+    data.forEach((e, index) => {
+      var ctx = document.getElementById('myChart' + index).getContext('2d');
+      var dataTemplate = prepareTemplate(e)
+      prepareChart(ctx, dataTemplate)
+    });
+  }
+  catch (e) {
+    console.log('Error!', e);
+  }
 }
 
 function prepareChart(ctx, data) {
@@ -40,3 +41,5 @@ function prepareTemplate(input) {
     },
   };
 }
+
+exec()
