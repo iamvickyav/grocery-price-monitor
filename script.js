2ea06433a7768c69a1935de5bbe6e34ceeca0951
Vicky AV <vickyavw.10@gmail.com>
2022-07-21 20:41:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 async function exec() {
   try {
-     const first = await fetch('./data.json'); //<-- wait for the fetch to finish and return the resolved value. 
-     let data = await first.json(); //<-- use the const first, not first_name
-     data.forEach((e, index) => {
-      var ctx = document.getElementById('myChart' + index).getContext('2d');
-      var dataTemplate = prepareTemplate(e)
-      prepareChart(ctx, dataTemplate)
-    });
+      const first = await fetch('./data.json'); //<-- wait for the fetch to finish and return the resolved value. 
+      let data = await first.json(); //<-- use the const first, not first_name
+    
+      var htmlElements = "";
+      for (var i = 0; i < array.length; i++) {
+        htmlElements += "<div class=\"col-md-4 col-xs-6\"><canvas id=\"myChart" + i + "\" width=\"300\" height=\"300\"></canvas></div>";
+      }
+      var container = document.getElementById("row");
+      container.innerHTML = htmlElements;
+      data.forEach((e, index) => {
+        var ctx = document.getElementById('myChart' + index).getContext('2d');
+        var dataTemplate = prepareTemplate(e)
+        prepareChart(ctx, dataTemplate)
+      });
    }
    catch(e) {
      console.log('Error!', e);
